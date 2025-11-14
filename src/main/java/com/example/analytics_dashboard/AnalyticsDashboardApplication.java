646bd40c773f572e49de72f3dd418363c04e6ad0
Matey42 <4matey2@gmail.com>
2025-11-14 17:57:29 +0100

    Init commit

---
+++ src/main/java/com/example/analytics_dashboard/AnalyticsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.analytics_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/analytics_dashboard/AnalyticsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analytics_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
