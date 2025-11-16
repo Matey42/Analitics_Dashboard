9fdb137ebd31e2e2b9b47378ff3bce236f3f6bdf
Matey42 <4matey2@gmail.com>
2025-11-16 19:47:24 +0100

    Update QueryController - /execute

--- src/main/java/com/example/analytics_dashboard/controller/QueryController.java
+++ src/main/java/com/example/analytics_dashboard/controller/QueryController.java
@@ -1,11 +1,15 @@
 package com.example.analytics_dashboard.controller;
 
+import com.example.analytics_dashboard.dto.ExecutionResponse;
 import com.example.analytics_dashboard.dto.QueryListResponse;
 import com.example.analytics_dashboard.dto.QueryRequest;
 import com.example.analytics_dashboard.dto.QueryResponse;
+import com.example.analytics_dashboard.model.StoredQuery;
+import com.example.analytics_dashboard.service.QueryExecutionService;
 import com.example.analytics_dashboard.service.QueryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +19,11 @@ import java.util.List;
 @RequestMapping("/queries")
 public class QueryController {
     private final QueryService queryService;
+    private final QueryExecutionService executionService;
 
-    public QueryController(QueryService queryService) {
+    public QueryController(QueryService queryService, QueryExecutionService executionService) {
         this.queryService = queryService;
+        this.executionService = executionService;
     }
 
     @PostMapping
@@ -30,6 +36,20 @@ public class QueryController {
     public ResponseEntity<List<QueryListResponse>> getAllQueries() {
         List<QueryListResponse> queries = queryService.getAllQueries();
         return ResponseEntity.ok(queries);
+    }
+
+    @GetMapping("/execute")
+    public ResponseEntity<List<List<Object>>> executeQuery(@RequestParam("query") Long queryId) {
+        StoredQuery storedQuery = queryService.getQueryById(queryId);
+
+        if(!executionService.isReadOnlyQuery(storedQuery.getQueryText())){
+            throw new IllegalArgumentException("Only Select queries are allowed");
+        }
+
+        List<List<Object>> result = executionService.executeQuery(storedQuery.getQueryText());
+
+        // ExecutionResponse response = new ExecutionResponse(result);
+        return ResponseEntity.ok(result);
     }
 
 }
