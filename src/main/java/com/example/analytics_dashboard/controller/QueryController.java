7f9a3aa50240e83eeced2e29d89f67cb1835af82
Matey42 <4matey2@gmail.com>
2025-11-22 22:17:55 +0100

    Update QueryController (not working)

--- src/main/java/com/example/analytics_dashboard/controller/QueryController.java
+++ src/main/java/com/example/analytics_dashboard/controller/QueryController.java
@@ -1,12 +1,12 @@
 package com.example.analytics_dashboard.controller;
 
-import com.example.analytics_dashboard.dto.ExecutionResponse;
-import com.example.analytics_dashboard.dto.QueryListResponse;
-import com.example.analytics_dashboard.dto.QueryRequest;
-import com.example.analytics_dashboard.dto.QueryResponse;
+import com.example.analytics_dashboard.dto.*;
 import com.example.analytics_dashboard.model.StoredQuery;
+import com.example.analytics_dashboard.service.AsyncQueryExecutionService;
 import com.example.analytics_dashboard.service.QueryExecutionService;
 import com.example.analytics_dashboard.service.QueryService;
+import com.example.analytics_dashboard.model.QueryExecution;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -20,10 +20,15 @@ import java.util.List;
 public class QueryController {
     private final QueryService queryService;
     private final QueryExecutionService executionService;
+    private final AsyncQueryExecutionService asyncExecutionService;
 
-    public QueryController(QueryService queryService, QueryExecutionService executionService) {
+
+    public QueryController(QueryService queryService,
+                           QueryExecutionService executionService,
+                           AsyncQueryExecutionService asyncExecutionService) {
         this.queryService = queryService;
         this.executionService = executionService;
+        this.asyncExecutionService = asyncExecutionService;
     }
 
     @PostMapping
@@ -48,6 +53,42 @@ public class QueryController {
 
         List<List<Object>> result = executionService.executeQuery(storedQuery.getQueryText());
         ExecutionResponse response = new ExecutionResponse(result);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/execute/async")
+    public ResponseEntity<AsyncExecutionResponse> executeQueryAsync(@RequestParam("query") Long queryId) {
+        StoredQuery storedQuery = queryService.getQueryById(queryId);
+
+        if(!executionService.isReadOnlyQuery(storedQuery.getQueryText())){
+            throw new IllegalArgumentException("Only SELECT queries are allowed");
+        }
+
+        String executionId = asyncExecutionService.startExecution((storedQuery));
+
+        return ResponseEntity.accepted()
+                .body(new AsyncExecutionResponse(executionId, "PENDING"));
+    }
+
+    @GetMapping("/execute/async/{executionId}")
+    public ResponseEntity<AsyncExecutionStatusResponse> getExecutionStatus(@PathVariable("executionId") String executionId) {
+        QueryExecution execution = asyncExecutionService.getExecution(executionId);
+
+        AsyncExecutionStatusResponse response = new AsyncExecutionStatusResponse(
+                execution.getId().toString(),
+                execution.getStatus().toString()
+        );
+
+        if (execution.getStatus() == QueryExecution.ExecutionStatus.COMPLETED) {
+            try {
+                response.setResult(asyncExecutionService.parseResult(execution.getResult()));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error parsing result", e);
+            }
+        } else if (execution.getStatus() == QueryExecution.ExecutionStatus.FAILED) {
+            response.setError(execution.getErrorMessage());
+        }
 
         return ResponseEntity.ok(response);
     }
